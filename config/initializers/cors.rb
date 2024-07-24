c90aca2778f23a14231a3c8cde5fbf847ff6b90c
awulf0 <andrewdwulf@gmail.com>
2024-07-24 16:14:29 -0500

    add render url to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   #let's see what happens
   allow do
-    origins "http://localhost:5173", "http://andrew-wulf-portfolio.com"
+    origins "http://localhost:5173", "http://andrew-wulf-portfolio.com", "https://andrew-wulf-portfolio-frontend.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
