ed60be5dff520a3ab0256cb783070a09feabdb7b
andrew wulf <andrewdwulf@gmail.com>
2025-03-22 20:10:41 -0500

    updated cors portfolio url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   #let's see what happens
   allow do
-    origins "http://localhost:5173", "https://www.andrew-wulf-portfolio.com", "https://twitter-clone-frontend-q1pw.onrender.com"
+    origins "http://localhost:5173", "https://andrew-wulf-portfolio.com/", "https://twitter-clone-frontend-q1pw.onrender.com"
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
